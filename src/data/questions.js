9e8b8715eee19679e38018d078e8e099deefe875
Nataliya Kim <magistr1982@gmail.com>
2024-06-07 16:14:03 +0300

    Complete Questions section styles

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,33 +1,41 @@
 export const questions = [
   {
+    id: '1',
     title: 'Scope',
     text: 'The scope of the car rental agreement defines the terms and conditions under which the rental service is provided, including the duration, permitted usage, and any limitations or restrictions imposed on the rental vehicle',
   },
   {
+    id: '2',
     title: 'Vehicle characteristics',
     text: 'Includes detailed information about the rental vehicle, such as its make, model, year, mileage, fuel type, and any additional features or accessories that come with the car.',
   },
   {
+    id: '3',
     title: 'Warranty / guarantee',
     text: "The statutory warranty is completely excluded to the extent permitted by law. If the vehicle still has a current factory warranty, a sales representative (brand representative) of the manufacturer shall provide the services owed therein. If there is a factory warranty, the beginning of the warranty for new vehicles or vehicles with a current manufacturer's warranty is not the date of the first placing on the market, but the beginning of the warranty in the manufacturer's system or the contractually agreed beginning of the warranty, if any. If there is a special warranty insurance for the vehicle, this will take the place of the material warranty according to section 3.1 and replace it. In this case, the General Terms and Conditions of the respective warranty service provider apply.",
   },
   {
+    id: '4',
     title: 'Trade-in vehicle',
     text: 'Customers can trade in their old vehicle as part of the rental agreement, which can help reduce the overall rental cost. The trade-in vehicles value is assessed based on its condition, age, and market value',
   },
   {
+    id: '5',
     title: 'Home Delivery',
     text: 'For convenience, some car rental services offer home delivery options, allowing customers to have the rental car delivered directly to their doorstep and picked up from their location at the end of the rental period.',
   },
   {
+    id: '6',
     title: 'Right of return',
     text: 'The right of return policy outlines the conditions under which a customer can return the rental vehicle earlier than the agreed rental period, including any potential fees or penalties that may apply.',
   },
   {
+    id: '7',
     title: 'Written form',
     text: 'All terms, conditions, and agreements related to the car rental must be documented in written form to ensure clarity and legal enforceability for both the rental company and the customer.',
   },
   {
+    id: '8',
     title: 'Applicable law and place of jurisdiction',
     text: 'This section specifies the legal framework governing the car rental agreement, including the applicable laws and the designated place of jurisdiction for resolving any disputes that may arise.',
   },
--- src/js/questionsMarkup.js
+++ src/js/questionsMarkup.js
@@ -6,12 +6,16 @@ questions.forEach(question => {
   const liElement = document.createElement('li');
   liElement.classList.add('quest-item');
   liElement.innerHTML = `
-    <h4 class="quest-subtitle">${question.title}</h4>
-    <p class="quest-text hidden">${question.text}</p>
+  <div  class="quest-wrapper">
+    <h4 class="quest-subtitle">${question.id}. ${question.title}</h4>
     <button class="quest-btn" type="button">
        <span class="plus">+</span><span class="minus hidden">-</span>
     </button>
-    <hr>
+  
+</div>
+<div class="hidden">
+    <p class="quest-text">${question.text}</p>
+</div>
   `;
 
   questionsListEl.appendChild(liElement);
@@ -19,12 +23,14 @@ questions.forEach(question => {
 
 document.querySelectorAll('.quest-btn').forEach(button => {
   button.addEventListener('click', function () {
-    const p = this.previousElementSibling;
+    const textDiv = this.parentElement.nextElementSibling;
     const plus = this.querySelector('.plus');
     const minus = this.querySelector('.minus');
 
-    p.classList.toggle('hidden');
+    textDiv.classList.toggle('hidden');
     plus.classList.toggle('hidden');
     minus.classList.toggle('hidden');
+
+    this.closest('.quest-item').classList.toggle('expanded');
   });
 });
